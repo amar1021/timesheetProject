a24f3885f8d0f032f822f882be15eba4b2930c2b
Thakare <amar.thakare@atos.net>
2019-05-30 00:01:37 +0530

    Grid Added

--- src/app/buglist/buglist.component.ts
+++ src/app/buglist/buglist.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { AgGridNg2 } from 'ag-grid-angular';
 import { Defect } from 'app/common/pojo/defect';
 import { DefectService } from 'app/common/services/defect/defect.service';
+import { PartialMatchFilter } from 'app/common/filter/PartialMatchFilter';
 
+declare var $: any;
 @Component({
   selector: 'app-buglist',
   providers: [DefectService],
@@ -12,23 +14,48 @@ import { DefectService } from 'app/common/services/defect/defect.service';
 export class BuglistComponent implements OnInit {
   @ViewChild('agGrid') agGrid: AgGridNg2;
   rowData = [];
+  private modelState = false;
+  private frameworkComponents;
+private gridApi;
+  gridOptions = {
+      columnDefs: 'columnDefs',
+      rowData: 'rowData',
+      rowSelection: 'single',
+      getSelectedRows: 'getSelectedRows',
+      onSelectionChanged: 'onSelectionChanged',
+      onRowDoubleClicked: 'doSomething(event)'
+  };
 
-  constructor(private defectService:DefectService) { }
+  constructor(private defectService:DefectService) {
+    this.frameworkComponents = { partialMatchFilter: PartialMatchFilter };
+   }
   columnDefs = [
-    {headerName: 'Defect ID', field: 'defect_id', sortable: true, filter: true, checkboxSelection: true },
-    {headerName: 'Title', field: 'defect_title', sortable: true, filter: true },
+    {headerName: 'Defect ID', field: 'defect_id', sortable: true, filter: "true", checkboxSelection: true },
+    {headerName: 'Title', field: 'defect_title', sortable: true, filter: "partialMatchFilter" },
     {headerName: 'Status', field: 'defect_status', sortable: true, filter: true },
     {headerName: 'Assign To', field: 'assign_to', sortable: true, filter: true },
     {headerName: 'Detected By', field: 'detected_by', sortable: true, filter: true },
     {headerName: 'Detected Date', field: 'detected_date', sortable: true, filter: true },
     {headerName: 'Comments', field: 'comments', sortable: true, filter: true }
 ];
-	// rowData = [
-	// 	{ make: 'Toyota', model: 'Celica', price: 35000 },
-	// 	{ make: 'Ford', model: 'Mondeo', price: 32000 },
-	// 	{ make: 'Porsche', model: 'Boxter', price: 72000 }
-  // ];
-  
+
+  OnGridReady(event: any){
+    this.gridApi = event.api;
+    // alert('I did something'+event)
+}
+onCellDoubleClicked(event: any) { 
+    console.log(event.data);
+    $("#myModal").modal('show');
+  /*   var selectedRows = this.gridApi.getSelectedRows();
+    var selectedRowsString = "";
+    selectedRows.forEach(function(selectedRow, index) {
+      if (index !== 0) {
+        selectedRowsString += ", ";
+      }
+      selectedRowsString += selectedRow.athlete;
+    }); */
+   }
+
   ngOnInit() {
     this.defectService.getDefectList()
     .subscribe(data =>
